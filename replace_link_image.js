2d0da003bfb0ec72f4366de0c6e27de72527c5d3
nguyenthixuandev <68955430+nguyenthixuandev@users.noreply.github.com>
2020-10-14 22:23:37 +0700

    Create replace_link_image.js
    Change url when the image has link

---
+++ replace_link_image.js
@@ -0,0 +1,44 @@
+
+</script>
+<script type="application/javascript">
+                jQuery(document).ready(function(){
+                    function getParamFromURL(t)
+                    {
+                        var a=new RegExp("[?&]"+t+"=([^&#]*)").exec(window.location.href);
+                        return null===a?null:a[1]
+                    }
+
+                    setTimeout(function(){
+                        if(jQuery('a.tcb-button-link').length > 0){
+                            const param = getParamFromURL('utm_campaign');
+                            if(param != null){
+                                let url = jQuery('a.tcb-button-link').attr('href');
+                                let new_url = '';
+                                if(url.indexOf('/?') < 0){
+                                     new_url = url + '/?tid=' + param
+                                }else{
+                                     new_url = url + '&tid=' + param
+                                }
+
+                                jQuery('a.tcb-button-link').attr('href', new_url);
+                            }
+                        }
+
+                        if(jQuery('.tve_image_frame > a').length > 0){
+                            const param = getParamFromURL('utm_campaign');
+                            if(param != null){
+                                let url = jQuery('.tve_image_frame > a').attr('href');
+                                let new_url = '';
+                                if(url.indexOf('/?') < 0){
+                                     new_url = url + '/?tid=' + param
+                                }else{
+                                     new_url = url + '&tid=' + param
+                                }
+
+                                jQuery('.tve_image_frame > a').attr('href', new_url);
+                            }
+                        }
+
+                    }, 1000);
+                });
+            </script>
